39724529644d069e9bed41ceab37968e81e1f102
Marina Rumyansev <aniramrum@gmail.com>
2018-09-06 18:15:31 +0300

    API GitSearchService to search libraries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from './git-search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GitSearchService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // tslint:disable-next-line:no-shadowed-variable
+  constructor(private GitSearchService: GitSearchService) {
+
+  }
   title = 'GitHub Browser';
+  ngOnInit() {
+    this.GitSearchService.gitSearch('angular').then((response) => {
+      alert('Total Libraries Found: ' + response.total_count);
+    }, (error) => {
+      alert('Error: ' + error.statusText);
+    });
+  }
 }
---
+++ src/app/git-search.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { GitSearch } from './git-search';
+import { HttpClient } from '@angular/common/http';
+// importing RxJS .toPromise to have a possibility to convert
+// an Observable received from httpRequest to promise.
+// import 'rxjs/add/operator/toPromise'; // toPromise is not an operator but a method on the Observable class so no need to import it
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GitSearchService {
+  // setting up a cache for first time service injection that will store cached values
+  cachedValues: Array<{
+    // describing shape of the objects we are going to store in the Array
+    // [key - query passed, type string]: value-actual output from the API - type of GitSearch
+    // such a way to create a cache will save cached data forever! If there is need to work with a frequently changed data
+    // this approach should be extended(check if expired, discarding data upon certain time etc.)
+    [query: string]: GitSearch
+  }> = [];
+  constructor(private http: HttpClient) { }
+    // creating a search method. To use it within a service using this.gitSearch, from outside of the service
+    // supposed to be referenced as GitSearchService.gitSearch
+    gitSearch = (query: string): Promise<GitSearch> => {
+      // tslint:disable-next-line:prefer-const
+      let promise = new Promise<GitSearch>((resolve, reject) => {
+
+        // checking if we want to use the cached value instead of querying for the response.
+        if (this.cachedValues[query]) {
+          resolve(this.cachedValues[query]);
+        } else {
+          // If a value isn't cached, we will return the call from the API.
+          // And converting a response from rxjs observable to Promise
+          this.http.get('https://api.github.com/search/repositories?q=' + query)
+          .toPromise()
+          .then( (response) => {
+            resolve(response as GitSearch);
+          }, (error) => {
+            reject(error);
+          });
+        }
+      });
+      return promise;
+    }
+}
